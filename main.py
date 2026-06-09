a11fde0731fd662c2624a902cd9a436904b7c5a0
Geringerleandro <geringerleandro@gmail.com>
2026-06-08 23:45:44 -0300

    Funcionalidad 1
    Agregado de la funcionalidad 1 del menú, permite ingresar un país con todos los datos necesarios.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from funciones import valida_nombre,valida_continente,numero_entero_positivo
 
+#Bloque 1: Carga del Dataset y construcción del diccionario
 
 diccionario_paises=[]
 with open ('paises.csv', 'r', encoding="utf-8" ) as archivo:
@@ -13,6 +14,31 @@ with open ('paises.csv', 'r', encoding="utf-8" ) as archivo:
             'continente':linea_procesada[3]
             }
         diccionario_paises.append(linea_paises)
+
+#Bloque 2: Lógica de las funcionalidades del menú
+
+def funcionalidad_1(diccionario):
+    pais_agregado=valida_nombre("Ingrese el nombre del país que desea agregar: ",diccionario)
+    poblacion_agregada=numero_entero_positivo("Ingrese la población: ")
+    superficie_agregada=numero_entero_positivo("Ingrese la superficie: ")
+    continente_agregado=valida_continente("Ingrese a que continente pertenece: ")
+    datos_agregados={
+        'nombre': pais_agregado,
+        'poblacion': poblacion_agregada,
+        'superficie': superficie_agregada,
+        'continente': continente_agregado
+            }
+    diccionario.append(datos_agregados)
+    print("País agregado correctamente.")
+    return
+
+def funcionalidad_2(diccionario):
+    pass
+
+def funcionalidad_3(diccionario):
+    pass
+
+#Bloque 3: Logica del menú
 
 ingreso_menu = True
 
@@ -30,7 +56,7 @@ while ingreso_menu:
 
     match opcion:
         case 1:
-            pass
+            funcionalidad_1(diccionario_paises)
         case 2:
             pass
         case 3:
