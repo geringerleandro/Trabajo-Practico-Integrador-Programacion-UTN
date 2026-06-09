e999f6f6fd5cfcab02c64ffb588b7d11ba5c6024
roudeairton <roudeairton@gmail.com>
2026-06-09 18:02:38 -0300

    funcionalidades(5/6) y se sumo try y except en el archivo csv
    se agregaron funcionalidades para las opciones 5 y 6 del menú con sus respectivas características y funciones, luego se agrego controladores de errores en el formato csv.

--- funciones.py
+++ funciones.py
@@ -79,3 +79,76 @@ def filtrar_por_superficie(paises, minimo, maximo):
             resultado.append(pais)
 
     return resultado
+
+def ordenar_paises(paises):
+    
+    paises_ordenados = sorted(paises, key=lambda pais: pais["nombre"])
+    
+    for pais in paises_ordenados:
+        print(pais["nombre"])
+
+def ordenar_por_poblacion(paises):
+    
+    paises_ordenados = sorted(paises, key=lambda pais: pais['poblacion'])
+
+    for pais in paises_ordenados:
+        print(f"{pais['nombre']}: {pais['poblacion']:,} habitantes.".replace(",", "."))
+
+def superficie_menor_a_mayor(paises):
+    
+    paises_ordenados = sorted(paises, key=lambda pais: pais["superficie"])
+
+    for pais in paises_ordenados:
+        print(f"{pais["nombre"]}: {pais["superficie"]:,} km² de superficie.")
+
+def superficie_mayor_a_menor(paises):
+    
+    paises_ordenados = reversed(sorted(paises, key=lambda pais: pais["superficie"]))
+
+    for pais in paises_ordenados:
+        print(f"{pais["nombre"]}: {pais["superficie"]:,} km² de superficie.")
+
+def mayor_y_menor_poblacion(paises):
+    pais_mas_poblado = max(paises, key=lambda pais: pais["poblacion"])
+    pais_menos_poblado = min(paises, key=lambda pais: pais["poblacion"])
+    
+    print (f"Pais mas poblado: {pais_mas_poblado['nombre']} {pais_mas_poblado['poblacion']:,} habitantes.".replace(",", "."))
+    print (f"Pais menos poblado: {pais_menos_poblado['nombre']} {pais_menos_poblado['poblacion']:,} habitantes".replace(",","."))
+
+def promedio_poblacion(paises):
+
+    for pais in paises:
+        densidad = pais['poblacion'] / pais['superficie']
+
+        print(f"{pais['nombre']}: {densidad:.2f} hab/km²")
+
+def promedio_superficie_continente(paises, continente):
+
+    suma_superficie = 0
+    cantidad_paises = 0
+
+    for pais in paises:
+        if pais['continente'].lower() == continente.lower():
+            suma_superficie += pais['superficie']
+            cantidad_paises += 1
+    
+    if cantidad_paises == 0:
+        return None
+    
+    return suma_superficie/cantidad_paises
+
+import unicodedata
+
+def quitar_acentos(texto):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', texto)
+        if unicodedata.category(c) != 'Mn')
+
+def paises_en_continente(paises, continente):
+    contador = 0
+    
+    for pais in paises:
+        if pais['continente'].lower() == continente.lower():
+            contador += 1
+    
+    return contador
